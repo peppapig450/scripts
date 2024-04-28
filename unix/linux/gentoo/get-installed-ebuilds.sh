cbd719095289bdf72f932d452d7b8b0b8e4cf378
peppapig450 <nbgenius1@gmail.com>
2024-04-28 16:02:58 -0700

    Initial commit with gentoo scripts

---
+++ unix/linux/gentoo/get-installed-ebuilds.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Get a list of installed packages and their corresponding installed files
+readarray -t packages < <(equery l -F '$cp' '*')
+readarray -t installed_ebuild_paths < <(equery  w "${packages[@]}")
+
+# Ouput the formatted output to a file
+for ((i = 0; i < ${#packages[@]}; i++)); do
+	echo "${packages[i]}:  ${installed_ebuild_paths[i]}"
+done > "$HOME/stuff/installed-package-ebuilds.txt"
