7b950d70d7d4be19a1acf09e0bc13afff8052d8c
peppapig450 <nbgenius1@gmail.com>
2024-04-30 13:28:51 -0700

    comment

--- unix/linux/gentoo/get-installed-ebuilds.sh
+++ unix/linux/gentoo/get-installed-ebuilds.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# TODO: ability to print to command line
 # Get a list of installed packages and their corresponding installed files
-readarray -t packages < <(equery l -F '$cp' '*')
+readarray -t packages < <(equery l -F '\$cp' '*')
 readarray -t installed_ebuild_paths < <(equery  w "${packages[@]}")
 
 # Ouput the formatted output to a file
