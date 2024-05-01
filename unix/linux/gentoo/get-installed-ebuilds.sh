d4e69efe05ff1afc57fc52570f315d80f01687ac
peppapig450 <nbgenius1@gmail.com>
2024-04-30 18:23:57 -0700

    remove backslash

--- unix/linux/gentoo/get-installed-ebuilds.sh
+++ unix/linux/gentoo/get-installed-ebuilds.sh
@@ -2,7 +2,7 @@
 
 # TODO: ability to print to command line
 # Get a list of installed packages and their corresponding installed files
-readarray -t packages < <(equery l -F '\$cp' '*')
+readarray -t packages < <(equery l -F '$cp' '*')
 readarray -t installed_ebuild_paths < <(equery  w "${packages[@]}")
 
 # Ouput the formatted output to a file
