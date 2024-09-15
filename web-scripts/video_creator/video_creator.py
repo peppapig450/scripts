6cbd139a5d544089d4b59256feca329b33026297
peppapig450 <nbgenius1@gmail.com>
2024-09-15 09:42:19 -0700

    start writing the conversion function

--- web-scripts/video_creator/video_creator.py
+++ web-scripts/video_creator/video_creator.py
@@ -8,3 +8,15 @@ import shutil
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+
+# TODO: separate function to create the down scaling filter
+def convert_video(input_file: str, output_base: str, height_width: tuple[int, int]):
+    vp9_file = f"{output_base}.webm"
+    h264_file = f"{output_base}.mp4"
+
+    try:
+        # Convert to VP9 WebM
+        logging.info(f"Converting {input_file} to VP9 (WebM)...")
+    except:
+        pass
